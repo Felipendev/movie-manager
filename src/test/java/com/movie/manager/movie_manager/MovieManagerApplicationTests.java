8a3e92266449e64777a022db7b4c170dda7f71e8
Felipe Pedroso <felipepedroso@sis-it.com>
2024-07-30 13:07:55 -0300

    feat: fist commit

---
+++ src/test/java/com/movie/manager/movie_manager/MovieManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.manager.movie_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
