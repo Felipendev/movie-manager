70e0a0770e4f35682cc03e383de2bfa1cf3f5889
Felipe Pedroso <felipepedroso@sis-it.com>
2024-07-31 13:40:11 -0300

    feat: implement controller SearchMovie and Service

--- src/main/java/com/movie/manager/movie_manager/movie/application/api/SearchMovieAPI.java
+++ src/main/java/com/movie/manager/movie_manager/movie/application/api/SearchMovieAPI.java
@@ -2,6 +2,7 @@ package com.movie.manager.movie_manager.movie.application.api;
 
 
 import com.movie.manager.movie_manager.movie.application.response.MovieResponse;
+import com.movie.manager.movie_manager.movie.application.service.SearchMovieService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Log4j2
 public class SearchMovieAPI {
 
+    private final SearchMovieService searchMovieService;
+
     @GetMapping("/{title}")
     public MovieResponse getMovieByTitle(@PathVariable String title) {
         log.info("[start] - SearchMovieAPI - getMovieByTitle");
         log.info("[title] {} ", title);
+        MovieResponse movie = searchMovieService.findMovieByTitle(title);                ;
         log.info("[finish] - SearchMovieAPI - getMovieByTitle");
-        return null;
+        return movie;
     }
 }
